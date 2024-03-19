05faa75e11526580406b53446c4a004cd6694ce1
Gabriel Retzlaff <93136568+R3TZgl@users.noreply.github.com>
2024-03-19 20:24:44 -0300

    aula 19/03/24

--- primeira_ativ.py
+++ primeira_ativ.py
@@ -33,3 +33,57 @@ print(f"A área do círculo é {raio ** 2 * math.pi}")
 
 ### 7
 
+aresta = int(iput('Digite o valor da aresta do quadrado: '))
+print(f'O dobro da área do quadrado é {aresta ** 2 * 2}')
+
+### 8
+
+pHora = int(input('Digite quanto você ganha por hora: '))
+horas = int(input('Digite quantas horas você trabalha por mês: '))
+
+print(f'Você irá ganhar R${phora * horas:.2f} esse mês. ')
+
+### 9 
+
+f = int(input('Digite a temperatura em °F: '))
+print(f'{f}°F são {(f - 32) / 1,8:.0f}')
+
+### 10
+
+c = int(input('Digite a temperatura em °C: '))
+print(f'{c}°C são {c * 1.8 - 32}°F')
+
+### 11
+
+altura = int(input('Digite a sua altura e cm: '))
+print(f'Seu peso ideal é de {(72*altura)-58}kg')
+
+### 12
+
+h = int(input('Digite a sua altura e cm: '))
+print(f'Se você for homem o seu peso ideal é de {(72*h)-58}kg/nSe você for mulher seu peso ideal é de {(62.1*h)-44.7}')
+
+### 13
+
+exesso = 0
+multa = 0
+
+peso = int(input('Digite o peso em kg: '))
+
+if peso > 50:
+    exesso = peso - 50
+    multa = exesso * 4
+
+print(f'Houve um exesso de {exesso}kg e terá de pagar uma multa de R${multa:.2f}')
+
+### 14
+
+
+
+### ATIV
+
+import math
+
+num = int(input('Digite um número que possue uma raíz quadrada inteira: '))
+
+print(f'A raíz quadrada de {num} é {math.sqrt(num)}')
